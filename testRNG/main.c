8363cafb6fa605632406545b263ae263ff449f0d
Alex Lelievre <alex@folabs.com>
2021-05-31 11:30:35 -0700

    Initial Commit

---
+++ testRNG/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  testRNG
+//
+//  Created by Alex Lelievre on 5/31/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
